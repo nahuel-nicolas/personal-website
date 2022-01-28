24da42fbb996e5fb72ee080d51ab0d3cb739ebfd
Nahuel Nicolas Barbieri <nnahuel012@gmail.com>
2022-01-28 20:27:16 -0300

    Update getUserCountry.js

--- scripts/functions/getUserCountry.js
+++ scripts/functions/getUserCountry.js
@@ -1,7 +1,7 @@
 // from https://stackoverflow.com/questions/3489460/how-to-get-visitors-location-i-e-country-using-geolocation
 
 // const ip_api = "https://ipinfo.io";
-const ip_api = 'https://api.ipregistry.co/?key=tryout';
+const ip_api = 'https://api.ipregistry.co/2802:8010:810e:6900:2d7d:9228:10b1:1f7b?key=qd3shi7mn54j38n1';
 
 export async function getUserCountry () {
     const userCountry = await fetch(ip_api)
