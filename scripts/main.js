dc0880cdf932065a1a65e1728de3f3e488a66220
Nahuel Nicolas Barbieri <nnahuel012@gmail.com>
2022-01-28 21:59:11 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,6 @@ import { getContactInfoTabContainer } from "./functions/getContactInfoTabContain
 import { loadContactInfoButtonInNavbar } from "./functions/loadContactInfoButtonInNavbar.js";
 import { loadMainContactLinksContainers } from "./functions/loadMainContactLinksContainers.js";
 import { loadSectionalWebElementsInSectionalWebContainers } from "./functions/loadSectionalWebElementsInSectionalWebContainers.js";
-import { getUserCountry } from "./functions/getUserCountry.js";
 
 const contactInfoTabContainer = getContactInfoTabContainer();
 loadContactInfoButtonInNavbar(contactInfoTabContainer);
